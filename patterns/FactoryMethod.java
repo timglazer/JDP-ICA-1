a43a9a1c849f9d8e609ab096268ec4aef3d48dbe
soojinjeong <107807010+soojinjeong@users.noreply.github.com>
2025-01-13 23:10:48 +0100

    Update FactoryMethod.java

--- patterns/FactoryMethod.java
+++ patterns/FactoryMethod.java
@@ -1,90 +1,10 @@
-// Abstract Device
-abstract class Device {
-    protected PhysicalLayer physicalLayer;
-    protected LinkLayer linkLayer;
-    protected Router router;
 
-    public Device(PhysicalLayer physicalLayer, LinkLayer linkLayer, Router router) {
-        this.physicalLayer = physicalLayer;
-        this.linkLayer = linkLayer;
-        this.router = router;
-    }
-
-    public void sendPacket(Packet packet) {
-        physicalLayer.send(packet);
-        linkLayer.send(packet);
-        router.route(packet);
-    }
-}
-
-// Physical Layer
-interface PhysicalLayer {
-    void send(Packet packet);
-}
-
-// Link Layer
-interface LinkLayer {
-    void send(Packet packet);
-}
 
 // Router
 interface Router {
     void route(Packet packet);
 }
 
-// WiFi Physical Layer
-class WiFiPhysical implements PhysicalLayer {
-    @Override
-    public void send(Packet packet) {
-        System.out.println("Packet passed through WiFi physical layer: " + packet.getContent());
-    }
-}
-
-// Ethernet Physical Layer
-class EthernetPhysical implements PhysicalLayer {
-    @Override
-    public void send(Packet packet) {
-        System.out.println("Packet passed through Ethernet physical layer: " + packet.getContent());
-    }
-}
-
-// WiFi Link Layer
-class WiFiLink implements LinkLayer {
-    @Override
-    public void send(Packet packet) {
-        System.out.println("Packet passed through WiFi link layer: " + packet.getContent());
-    }
-}
-
-// Ethernet Link Layer
-class EthernetLink implements LinkLayer {
-    @Override
-    public void send(Packet packet) {
-        System.out.println("Packet passed through Ethernet link layer: " + packet.getContent());
-    }
-}
-
-// Simple Router
-class SimpleRouter implements Router {
-    @Override
-    public void route(Packet packet) {
-        System.out.println("Packet routed by simple router: " + packet.getContent());
-    }
-}
-
-
-// Packet
-class Packet {
-    private String content;
-
-    public Packet(String content) {
-        this.content = content;
-    }
-
-    public String getContent() {
-        return content;
-    }
-}
 
 // Abstract Factory
 abstract class DeviceFactory {
